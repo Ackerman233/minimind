296714ee7c36e95af46faa3b41b17a9942dd829a
Ackerman233 <2046939717@qq.com>
2026-02-04 20:47:49 +0800

    RMSNorm编写

--- model/model.py
+++ model/model.py
@@ -69,3 +69,21 @@ class MokioMindConfig(PretrainedConfig):
             if self.inference_rope_scaling
             else None
         )
+ 
+import torch
+import torch.nn as nn
+#继承nn.Module类
+class RMSNorm(nn.Module):
+    
+#init初始化
+    def __init__(self, dim:int, eps:float=1e-5):
+        super().__init__()
+        self.dim = dim
+        self.eps = eps
+        self.weight = nn.Parameter(torch.ones(dim))
+#norm
+    def _norm(self,x):
+        return x * torch.rsqrt( x.pow(2).mean(-1,keepdim=True) + self.eps )
+#forward方法
+    def foward(self,x):
+        return self._norm(x.float()).type_as(x) * self.weight
