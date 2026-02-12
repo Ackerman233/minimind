2f8dcd9bdbe3048aa8749c8d16bf699231ef58bd
Ackerman233 <2046939717@qq.com>
2026-02-12 21:45:38 +0800

    修改sft训练代码，添加无MoE的情况；修改文件路径

--- trainer/train_sft.py
+++ trainer/train_sft.py
@@ -44,12 +44,14 @@ def train_epoch(epoch,loader,iters,start_step=0,wandb=None):
             
             # 损失计算
             loss=loss_fct(res.logits.view(-1,res.logits.size(-1)),Y.view(-1)).view(Y.size())
-            
+
             loss= (loss * loss_mask).sum() / loss_mask.sum()
-            
-            loss+=res.aux_loss
-            
-            loss=loss/args.acculation_steps
+
+            # MoE架构有aux_loss负载均衡损失
+            if hasattr(res, 'aux_loss'):
+                loss+=res.aux_loss
+
+            loss=loss/args.accumulation_steps
             
         scaler.scale(loss).backward()
         
@@ -94,7 +96,7 @@ def train_epoch(epoch,loader,iters,start_step=0,wandb=None):
             torch.save(state_dict, ckp)
             # 保存完整训练状态
             lm_checkpoint(lm_config, weight=args.save_weight, model=model, optimizer=optimizer, 
-                         epoch=epoch, step=step, wandb=wandb, save_dir='../checkpoints', scaler=scaler)
+                         epoch=epoch, step=step, wandb=wandb, save_dir='./checkpoints', scaler=scaler)
             model.train()  # 恢复训练模式
             
 if __name__ == "__main__":
@@ -111,7 +113,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="MiniMind Full SFT")
     
     # ========== 基础训练参数 ==========
-    parser.add_argument("--save_dir", type=str, default="../out", 
+    parser.add_argument("--save_dir", type=str, default="./out", 
                        help="模型保存目录")
     parser.add_argument('--save_weight', default='full_sft', type=str, 
                        help="保存权重的前缀名")
@@ -189,7 +191,7 @@ if __name__ == "__main__":
     # ========== 2. 配置目录、模型参数、检查ckp ==========
     os.makedirs(args.save_dir, exist_ok=True)
     lm_config = MokioMindConfig(hidden_size=args.hidden_size, num_hidden_layers=args.num_hidden_layers, use_moe=bool(args.use_moe))
-    ckp_data = lm_checkpoint(lm_config, weight=args.save_weight, save_dir='../checkpoints') if args.from_resume==1 else None
+    ckp_data = lm_checkpoint(lm_config, weight=args.save_weight, save_dir='./checkpoints') if args.from_resume==1 else None
     
     # ========== 3. 设置混合精度 ==========
     device_type = "cuda" if "cuda" in args.device else "cpu"
