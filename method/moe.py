5098c7d13e624ffc7e40b8c9500e30f324689080
Ackerman233 <2046939717@qq.com>
2026-03-15 21:21:10 +0800

    MoE前向传播代码编写

--- method/moe.py
+++ method/moe.py
@@ -13,3 +13,18 @@ out2 = torch.tensor(
 )
 print(out2.mean(dim=1))
 
+# moeFFN用的
+
+x=torch.tensor([1,2,3])
+print( torch.repeat_interleave(x,repeats=2) )
+
+
+y=torch.tensor([3,1,2])
+idx= torch.argsort(y)
+print(idx)
+print(y[idx])
+
+
+z=torch.tensor([0,1,1,3,2,1])
+count = torch.bincount(z)
+print(count)
